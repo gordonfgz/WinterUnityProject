4919c45c7ebb8051cc21225c21b5587e9d41490e
Carbon9000 <90561917+ProcessedCarbon@users.noreply.github.com>
2021-12-24 14:41:16 +0800

    Minor Changes
    Remove component requirement for abstract contact class
    
    Change projectile from diamond to circle

--- WinterUnityProject/Assets/Scripts/Contact_Trigger/AbstractContact.cs
+++ WinterUnityProject/Assets/Scripts/Contact_Trigger/AbstractContact.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(PolygonCollider2D))]
 public abstract class AbstractContact : MonoBehaviour
 {
     public string[] tags = { "Player" };
